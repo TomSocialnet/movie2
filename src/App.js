2ec4bbfb4819f47e519004b3f391843e2e98cdbd
Tom Tom <tom.socialnet@live.com>
2023-08-04 21:56:25 +0200

    ok2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactttt
+          Learn React (try again)
         </a>
       </header>
     </div>
